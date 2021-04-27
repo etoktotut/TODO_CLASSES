bcc464752f90d0a60bd7bc56a440ff361db93749
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-27 15:52:06 +0300

    anim and edit added

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ class Todo {
         this.todoCompleted = document.querySelector(todoCompleted);
         this.todoContainer = document.querySelector(todoContainer);
         this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
+        let isFocus = false;
     }
 
     addToStorage() {
@@ -33,6 +34,7 @@ class Todo {
         li.insertAdjacentHTML('beforeend', `
                 <span class="text-todo">${todo.value}</span>
 				<div class="todo-buttons">
+                    <button class="todo-edit"></button>
 					<button class="todo-remove"></button>
 					<button class="todo-complete"></button>
 				</div>
@@ -67,6 +69,56 @@ class Todo {
         return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     }
 
+    animate(li, updown, isDel, fun) {
+        let compress = 100;
+        let idFrame, idFrame1;
+        let step = 0;
+        const ulTop = li.parentNode.getBoundingClientRect().top;
+        const liTop = li.getBoundingClientRect().top;
+        const ulBtm = li.parentNode.getBoundingClientRect().bottom;
+        let marginBottom = isDel ? window.innerHeight - liTop : ulBtm - liTop;
+        let marginTop = liTop - ulTop;
+
+        const fallDown = () => {
+            step += isDel ? 30 : 10;
+            console.log(step);
+            li.style.top = step + 'px';
+            if (step > marginBottom) {
+                cancelAnimationFrame(idFrame1);
+                fun();
+                return;
+            }
+            idFrame1 = requestAnimationFrame(fallDown);
+        };
+
+        const fallUp = () => {
+            step -= 10;
+            li.style.top = step + 'px';
+            if (Math.abs(step) >= marginTop) {
+                cancelAnimationFrame(idFrame1);
+                fun();
+                return;
+            }
+            idFrame1 = requestAnimationFrame(fallUp);
+        };
+
+        const smoothCompress = () => {
+            compress -= 5;
+            li.style.width = compress + '%';
+            if (isDel) {
+                li.style.opacity = compress + '%';
+            }
+
+            if (compress <= 10) {
+                cancelAnimationFrame(idFrame);
+                updown ? fallUp() : fallDown();
+                return;
+            }
+            idFrame = requestAnimationFrame(smoothCompress);
+        };
+        smoothCompress();
+    }
+
     deleteItem(liDel) {
         for (let item of this.todoData) {
             if (item[0] === liDel.key) {
@@ -74,8 +126,10 @@ class Todo {
                 break;
             }
         }
-        this.render();
+        this.animate(liDel, false, true, this.render.bind(this));
+
     }
+
 
     completed(liComp) {
         for (let item of this.todoData) {
@@ -84,8 +138,47 @@ class Todo {
                 break;
             }
         }
-        this.render();
+        if (liComp.closest('ul').classList.contains('todo-completed')) {
+
+            this.animate(liComp, true, false, this.render.bind(this));
+
+        } else {
+
+            this.animate(liComp, false, false, this.render.bind(this));
+        }
+
     }
+
+    editItem(li) {
+
+        const todoText = li.querySelector('.text-todo');
+        todoText.setAttribute('contenteditable', 'true');
+        todoText.focus();
+
+        todoText.addEventListener('keydown', (e) => {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                todoText.setAttribute('contenteditable', 'false');
+            }
+        });
+
+        todoText.addEventListener('focusout', () => {
+            if (todoText.textContent.trim() === '') {
+                alert('Поле не может быть пустым');
+                this.render();
+                return;
+            }
+            for (let item of this.todoData) {
+                if (item[0] === li.key) {
+                    item[1].value = todoText.textContent;
+                    break;
+                }
+            }
+            todoText.setAttribute('contenteditable', 'false');
+            this.addToStorage();
+        });
+    }
+
 
     handler(e) {
         e.preventDefault();
@@ -96,6 +189,10 @@ class Todo {
 
         if (target.matches('.todo-complete')) {
             this.completed(target.closest('li'));
+        }
+
+        if (target.matches('.todo-edit')) {
+            this.editItem(target.closest('li'));
         }
     }
 
